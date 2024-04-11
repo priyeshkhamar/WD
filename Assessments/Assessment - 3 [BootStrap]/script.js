0378789db9c656b8935bd2b990ec57c583fff1cd
Priyesh Khamar <131170202+priyeshkhamar@users.noreply.github.com>
2024-04-11 12:20:41 +0530

    Assessment-3

--- Assessments/Assessment - 3 [BootStrap]/script.js
+++ Assessments/Assessment - 3 [BootStrap]/script.js
@@ -1,25 +1,32 @@
 $(document).ready(function () {
-  $(".datepicker").datepicker({ format: "dd/mm/yyyy", autoclose: true });
-
   $("#calculateButton").click(function () {
-    var fromDate = moment($("#fromDate").datepicker("getDate"));
-    var toDate = moment($("#toDate").datepicker("getDate"));
+    var fromDate = new Date($("#fromDate").val());
+    var toDate = new Date($("#toDate").val());
 
-    if (!fromDate.isValid() || !toDate.isValid()) {
-      alert("Please select both From and To dates.");
-      return;
-    }
-
-    var diff = toDate.diff(fromDate);
+    var diffInMilliseconds = toDate - fromDate;
+    var diffInDays = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24));
+    var diffInWeeks = Math.floor(diffInDays / 7);
+    var diffInMonths =
+      toDate.getMonth() -
+      fromDate.getMonth() +
+      12 * (toDate.getFullYear() - fromDate.getFullYear());
+    var diffInYears = toDate.getFullYear() - fromDate.getFullYear();
 
     $("#results").html(`
-      <p>Date interval between ${fromDate.format(
-        "DD/MM/YYYY"
-      )} and ${toDate.format("DD/MM/YYYY")}</p>
-      <p>In years: ${toDate.diff(fromDate, "years")}</p>
-      <p>In months: ${toDate.diff(fromDate, "months")}</p>
-      <p>In weeks: ${toDate.diff(fromDate, "weeks")}</p>
-      <p>In days: ${toDate.diff(fromDate, "days")}</p>
+      <p>Date interval between ${formatDate(
+        fromDate
+      )} and ${formatDate(toDate)}</p>
+      <p>In years: ${diffInYears}</p>
+      <p>In months: ${diffInMonths}</p>
+      <p>In weeks: ${diffInWeeks}</p>
+      <p>In days: ${diffInDays}</p>
     `);
   });
+
+  function formatDate(date) {
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
 });
